2f2d73dfd3b29ad2f5e89a60fbba84bf7bc8b110
abhishekra07 <abhira719@gmail.com>
2023-02-22 14:00:43 +0530

    Update ThreadSafeSingleton.java

--- src/com/java/design/pattern/creational/singleton/ThreadSafeSingleton.java
+++ src/com/java/design/pattern/creational/singleton/ThreadSafeSingleton.java
@@ -11,4 +11,15 @@ public class ThreadSafeSingleton {
         }
         return instance;
     }
+    //double-checked locking
+    public static ThreadSafeSingleton getInstanceUsingDoubleLocking() {
+        if (instance == null) {
+            synchronized (ThreadSafeSingleton.class) {
+                if (instance == null) {
+                    instance = new ThreadSafeSingleton();
+                }
+            }
+        }
+        return instance;
+    }
 }
